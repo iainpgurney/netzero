3b224d7ab0450c3c1e796d1c307702b02990a40b
Carma <113047481+carma-earth@users.noreply.github.com>
2025-11-19 20:09:03 +0000

    rewrite error pages to fix build

---
+++ pages/500.js
@@ -0,0 +1,9 @@
+export default function Custom500() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>500 - Server Error</h1>
+      <p>Sorry, something went wrong on our end.</p>
+    </div>
+  );
+}
+
